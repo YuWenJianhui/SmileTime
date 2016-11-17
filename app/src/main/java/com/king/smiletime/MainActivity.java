29885625a6bf3c96a090d8e53ec9e153eb74bbe2
ChangKaiKing <785319091@qq.com>
2016-11-17 17:14:09 +0800

    添加Fragment 修改styles

--- app/src/main/java/com/king/smiletime/MainActivity.java
+++ app/src/main/java/com/king/smiletime/MainActivity.java
@@ -3,8 +3,11 @@ package com.king.smiletime;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.view.Window;
+
+import com.king.smiletime.scandal.FirstFragment;
 
 public class MainActivity extends FragmentActivity {
 
@@ -17,7 +20,10 @@ public class MainActivity extends FragmentActivity {
           //不显示actionbar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.tdfl_id, new FirstFragment());
+        transaction.commit();
 
 
 
@@ -27,6 +33,9 @@ public class MainActivity extends FragmentActivity {
     public void BtnAction(View view) {
         switch (view.getId()) {
             case R.id.scandal_id:
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.tdfl_id, new FirstFragment());
+                transaction.commit();
                 break;
             case R.id.friend_id:
 
