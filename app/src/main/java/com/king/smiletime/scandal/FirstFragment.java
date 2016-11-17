2324147aec7258345f71d44faf3e0961def89b3e
ChangKaiKing <785319091@qq.com>
2016-11-17 17:03:10 +0800

    添加Fragment 修改styles

--- app/src/main/java/com/king/smiletime/scandal/FirstFragment.java
+++ app/src/main/java/com/king/smiletime/scandal/FirstFragment.java
@@ -2,11 +2,21 @@ package com.king.smiletime.scandal;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
 import com.king.smiletime.R;
+import com.king.smiletime.friends.FriendsFragment;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2016/10/27.
@@ -14,7 +24,14 @@ import com.king.smiletime.R;
 
 public class FirstFragment extends Fragment {
 
-
+    private TabLayout mTab;
+    private ViewPager mVp;
+    private FragmentStatePagerAdapter adapter;
+    private String[] tbname;
+    private String[] urls;
+    private List<String> mTblist;
+    private List<Fragment> fragments;
+    private ImageView mImg;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,8 +41,68 @@ public class FirstFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.firstfragment, null);
-
+        mTab = (TabLayout) view.findViewById(R.id.tab_id);
+        mVp = (ViewPager) view.findViewById(R.id.vpger_id);
+        //创建数据源
+        initList();
+        //添加viewpager数据
+        addViewList();
+        mTab.setupWithViewPager(mVp);
+        mTab.setTabMode(TabLayout.MODE_FIXED);
+        mTab.setupWithViewPager(mVp);
         return view;
+    }
+
+    private void addViewList() {
+        //数据源
+        fragments = new LinkedList<>();
+        int i = 0;
+        for (String tabName : tbname) {
+            JokesFragment fragment = new JokesFragment();
+            Bundle args = new Bundle();
+            args.putString("url", urls[i++]);
+            fragment.setArguments(args);
+            fragments.add(fragment);
+        }
+        //适配器
+        adapter = new FragmentStartPagerAdapter(getFragmentManager());
+        //绑定
+        mVp.setAdapter(adapter);
+    }
+
+    private void initList() {
+        tbname = getResources().getStringArray(R.array.tbname);
+        mTblist = new LinkedList<>();
+        int i = 0;
+        while (i < tbname.length) {
+            mTblist.add(tbname[i]);
+            i++;
+        }
+        urls = getResources().getStringArray(R.array.url);
+    }
+
+    public class FragmentStartPagerAdapter extends FragmentStatePagerAdapter {
+
+        public FragmentStartPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        //此处用来定制Fragment的标题，就是定制TabLayout中每个Tab的名字
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTblist.get(position);
+        }
     }
 
 }
