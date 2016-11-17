5b6cc5bc59ffdb9432fdb38ee26b6a8b52401ead
ChangKaiKing <785319091@qq.com>
2016-11-17 13:43:32 +0800

    添加Fragment

---
+++ app/src/main/java/com/king/smiletime/video/VideoFragment.java
@@ -0,0 +1,10 @@
+package com.king.smiletime.video;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 2016/11/17.
+ */
+
+public class VideoFragment extends Fragment {
+}
