4600abd66fff65904632b96fdd9b2ddc75feeca2
Administrator <1149078170@qq.com>
2016-11-21 09:40:02 +0800

    添加了管理糗事和动态的页面视图

--- app/src/main/java/com/king/smiletime/my/MyFragment.java
+++ app/src/main/java/com/king/smiletime/my/MyFragment.java
@@ -7,14 +7,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.king.smiletime.R;
+
 /**
  * Created by Administrator on 2016/11/17.
  */
 
 public class MyFragment extends Fragment {
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_my, null);
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
     }
 }
