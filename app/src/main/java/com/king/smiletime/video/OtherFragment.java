9ab1b9a40a58b8f3bc12ab3e529957fa216c646d
YuWenJianhui <1095843560@qq.com>
2016-11-22 09:25:52 +0800

    修改三模块

---
+++ app/src/main/java/com/king/smiletime/video/OtherFragment.java
@@ -0,0 +1,105 @@
+package com.king.smiletime.video;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.king.asynctask.DownloadJsonDataAysncTask;
+import com.king.entity.Banners;
+import com.king.entity.Person;
+import com.king.smiletime.R;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/11/17.
+ */
+
+public class OtherFragment extends Fragment implements DownloadJsonDataAysncTask.JsonDataSettingCallBack{
+
+    private View view;
+    private ListView listView;
+    private TextView replace;
+    private String tabName;
+    private String url;
+    private ViewPager viewPager;
+    private ImageView imageView;
+    private List<String> ds;
+    private Person person;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        Bundle bundle = getArguments();
+        tabName = bundle.getString("tabName");
+        url =   bundle.getString("url");
+        Log.i("msg",url);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_other, null);
+        listView = (ListView) view.findViewById(R.id.listView);
+        replace = (TextView) view.findViewById(R.id.tv_replace_id);
+        listView.setEmptyView(replace);
+
+        //关于ListView
+        aboutlistView();
+        return view;
+    }
+
+    private void getData() {
+        ProgressDialog dialog = new ProgressDialog(getContext());
+       new DownloadJsonDataAysncTask(this,dialog).execute(url);
+
+    }
+
+
+
+    private void fillData() {
+        //获取json数据
+        getData();
+
+    }
+
+    @Override
+    public void setJsonData(Person result) {
+        Log.i("msg3",result.toString());
+        List<Banners> banners = result.getBanners();
+        for(Banners banner:banners){
+            String url = banner.getUrl();
+            Log.i("msg2",url);
+            ds.add(url);
+        }
+
+
+    }
+
+
+    private void aboutlistView() {
+        //数据源
+        //适配器
+        //绑定
+        //监听器
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+
+
+
+}
