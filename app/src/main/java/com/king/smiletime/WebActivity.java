8213510d367ba8f05d25858b193c33228887e925
YuWenJianhui <1095843560@qq.com>
2016-11-24 21:17:15 +0800

    修改三模块

---
+++ app/src/main/java/com/king/smiletime/WebActivity.java
@@ -0,0 +1,27 @@
+package com.king.smiletime;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class WebActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webactivity);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("热猫直播公约");
+
+        ImageView image = (ImageView) findViewById(R.id.imv_id);
+        String imageUrl = getIntent().getStringExtra("imageUrl");
+        Picasso.with(this).load(imageUrl).into(image);
+    }
+}
