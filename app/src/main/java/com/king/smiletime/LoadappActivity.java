f85646c23485f4ba4497a6a201e42fa6528640ae
ChangKaiKing <785319091@qq.com>
2016-11-30 12:17:10 +0800

    上传资源文件

--- app/src/main/java/com/king/smiletime/LoadappActivity.java
+++ app/src/main/java/com/king/smiletime/LoadappActivity.java
@@ -1,23 +1,23 @@
 package com.king.smiletime;
 
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
-public class LoadappActivity extends Activity implements View.OnClickListener{
+public class LoadappActivity extends AppCompatActivity implements View.OnClickListener{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.appactivity);
-        getActionBar().setTitle("发起直播");
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        getActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("发起直播");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
         TextView load = (TextView) findViewById(R.id.tv_load);
         load.setOnClickListener(this);
     }
@@ -27,4 +27,17 @@ public class LoadappActivity extends Activity implements View.OnClickListener{
         Toast.makeText(this, "下载热猫直播app", Toast.LENGTH_SHORT).show();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==android.R.id.home){
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
